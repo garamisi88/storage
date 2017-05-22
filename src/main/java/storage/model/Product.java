8bfca82ccce9a57438ac1de2ac89c8de9f45853a
Misi <gara.mihaly@pwstudio.hu>
2017-05-22 23:56:18 +0200

    Product esetén az OrderItem-nél Lazy betöltés

--- src/main/java/storage/model/Product.java
+++ src/main/java/storage/model/Product.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -223,7 +224,7 @@ public class Product implements Serializable {
 	}
 	
 	
-	@OneToMany(mappedBy="product")
+	@OneToMany(mappedBy="product", fetch=FetchType.LAZY)
 	@Column(nullable=true)
 	public Collection<OrderItem> getOrderItems() {
 		return orderItems;
